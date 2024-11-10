9ddfbf2cf11255fb6985f5c5484e7db00653cb4f
piaa0802 <162582266+piaa0802@users.noreply.github.com>
2024-11-10 14:52:45 +0100

    task2 add and task1 update

---
+++ labs/03/task2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+$ wc -l /c/Uni/DIS08/*.tsv
+    13712 /c/Uni/DIS08/2014-01-31_JA-africa.tsv
+    27392 /c/Uni/DIS08/2014-01-31_JA-america.tsv
+   507732 /c/Uni/DIS08/2014-01_JA.tsv
+     5375 /c/Uni/DIS08/2014-02-02_JA-britain.tsv
+   554211 total
